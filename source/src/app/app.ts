b2d8a2fef610d637c5c5dffcedb39fc8cf0f7f38
Lakhveer Singh <lakhveer@Lakhveers-Mac-mini.local>
2025-06-27 09:34:44 +0530

    Cat with cursor

--- source/src/app/app.ts
+++ source/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, viewChild } from '@angular/core';
+import { createAnimatable, utils } from 'animejs';
 import { Subscription } from 'rxjs';
 import { About } from './components/about/about';
 import { Education } from './components/education/education';
@@ -23,7 +24,13 @@ import { ThemeService } from './services/theme.service';
   templateUrl: './app.html',
   styleUrl: './app.less'
 })
-export class App implements OnInit, OnDestroy {
+export class App implements OnInit, AfterViewInit, OnDestroy {
+  cat = viewChild<ElementRef<HTMLImageElement>>('cat');
+  catContainer = viewChild<ElementRef<HTMLDivElement>>('catContainer');
+  footerContainer = viewChild<ElementRef<HTMLDivElement>>('footerContainer');
+
+  catResting = false;
+
   subscription = new Subscription();
 
   constructor(
@@ -34,6 +41,61 @@ export class App implements OnInit, OnDestroy {
      this.subscription.add(this.themeService.getTheme().subscribe(theme => {
        document.body.classList.toggle('dark', theme === 'dark');
      }));
+  }
+
+  ngAfterViewInit(): void {
+    const cat = this.cat();
+    const catContainer = this.footerContainer();
+
+    if (!cat || !catContainer) {
+      console.error('Cat or Cat Container element not found');
+      return;
+    }
+
+    this.animateCat(cat.nativeElement, catContainer.nativeElement);
+  }
+
+  animateCat(cat: HTMLImageElement, catContainer: HTMLDivElement): void {
+    const animatableCat = createAnimatable(cat, {
+      x: 3000,
+      y: 3000,
+      ease: 'out(3)',
+    });
+    let bounds = catContainer.getBoundingClientRect();
+
+    const refreshBounds = () => bounds = catContainer.getBoundingClientRect();
+
+    const onMouseMove = (e: MouseEvent) => {
+      let x = 0;
+      let y = 0;
+      if (this.catResting) {
+        const { width, height } = bounds;
+        const hw = width - 48;
+        const hh = height - 24;
+        const top = catContainer.offsetTop;
+
+        x = utils.clamp(e.clientX, 0, hw);
+        y = utils.clamp(e.pageY, top, top + hh);
+
+      } else {
+        const { left, top } = {
+          left: window.scrollX,
+          top: window.scrollY
+        };
+
+        x = utils.clamp(e.clientX, 0, bounds.width - 48);
+        y = e.clientY + top;
+      }
+      animatableCat['x'](x);
+      animatableCat['y'](y);
+    }
+
+    window.addEventListener('mousemove', onMouseMove);
+    catContainer.addEventListener('scroll', refreshBounds);
+  }
+
+  toggleResting(): void {
+    this.catResting = !this.catResting;
   }
 
   ngOnDestroy(): void {
--- source/src/app/components/footer/footer.ts
+++ source/src/app/components/footer/footer.ts
@@ -1,50 +1,20 @@
-import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
-import { createAnimatable, utils } from 'animejs';
-
+import { CommonModule } from '@angular/common';
+import { Component, output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'sj-footer',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.less'
 })
-export class Footer implements AfterViewInit {
-  cat = viewChild<ElementRef<HTMLImageElement>>('cat');
-  catContainer = viewChild<ElementRef<HTMLDivElement>>('catContainer');
+export class Footer {
+  catResting = false;
+  catRestingEvent = output<boolean>();
 
-  ngAfterViewInit(): void {
-    const cat = this.cat();
-    const catContainer = this.catContainer();
-
-    if (!cat || !catContainer) {
-      console.error('Cat or Cat Container element not found');
-      return;
-    }
-
-    this.animateCat(cat.nativeElement, catContainer.nativeElement);
+  toggleResting(): void {
+    this.catResting = !this.catResting;
+    this.catRestingEvent.emit(this.catResting);
   }
 
-  animateCat(cat: HTMLImageElement, catContainer: HTMLDivElement): void {
-    const animatableCat = createAnimatable(cat, {
-      x: 500,
-      y: 500,
-      ease: 'out(3)',
-    });
-    let bounds = catContainer.getBoundingClientRect();
-
-    const refreshBounds = () => bounds = catContainer.getBoundingClientRect();
-
-    const onMouseMove = (e: MouseEvent) => {
-      const { width, height, left, top } = bounds;
-      const hw = width - 30;
-      const hh = (height / 2) - 24;
-
-      const x = utils.clamp(e.clientX - left, -24, hw);
-      const y = utils.clamp(e.clientY - top - hh, -hh, hh);
-      animatableCat['x'](x);
-      animatableCat['y'](y);
-    }
-
-    window.addEventListener('mousemove', onMouseMove);
-    catContainer.addEventListener('scroll', refreshBounds);
-  }
 }
