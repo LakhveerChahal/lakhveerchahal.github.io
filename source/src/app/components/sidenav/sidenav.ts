3d1251e64a45d3fee8948d05024b342d72b3fadd
Lakhveer Singh <lakhveer@Lakhveers-Mac-mini.local>
2025-06-29 14:43:04 +0530

    Added theme toggle in sidenav and cute little name in title bar :)

--- source/src/app/components/sidenav/sidenav.ts
+++ source/src/app/components/sidenav/sidenav.ts
@@ -1,10 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, output } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { IsActiveMatchOptions, RouterModule } from '@angular/router';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'sj-sidenav',
   imports: [
     RouterModule,
+    CommonModule,
+    MatSlideToggleModule,
   ],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.less'
@@ -18,6 +23,16 @@ export class Sidenav {
     paths: 'exact',
     fragment: 'exact',
   };
+
+  isDarkMode: boolean = true;
+
+  constructor(private themeService: ThemeService) {
+  }
+
+  toggleTheme(): void {
+    this.themeService.setTheme(this.isDarkMode ? 'light' : 'dark');
+    this.isDarkMode = !this.isDarkMode;
+  }
 
   emitToggleSideNav(): void {
     this.toggleSideNavEvent.emit();
